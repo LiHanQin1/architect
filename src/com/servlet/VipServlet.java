48fa59fc2cd57d27acc3020f6a398ba0bc6bc63b
HongSiDa <467650658@qq.com>
2020-12-10 15:38:35 +0800

    注册的前后端校验

--- src/com/servlet/VipServlet.java
+++ src/com/servlet/VipServlet.java
@@ -1,5 +1,6 @@
 package com.servlet;
 
+import com.entity.User;
 import com.entity.Vip;
 import com.service.VipService;
 import com.service.impl.VipServiceImpl;
@@ -11,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 /**
  * @Author HongSiDa
@@ -55,22 +59,39 @@ public class VipServlet extends BaseServlet {
         String qq=request.getParameter("qq");
         String email=request.getParameter("email");
         String sex=request.getParameter("sex");
-        if (password.equals(confirmpassword)){
-            Vip vip=new Vip();
-            vip.setUserName(username);
-            vip.setUserPwd(password);
-            vip.setTel(tel);
-            vip.setQQ(qq);
-            vip.setEmail(email);
-            vip.setSex(sex);
-            Integer result=vipService.insert(vip);
-            if (result==1){
+
+        Vip vip=new Vip();
+        vip.setUserName(username);
+        vip.setUserPwd(password);
+        vip.setTel(tel);
+        vip.setQQ(qq);
+        vip.setEmail(email);
+        vip.setSex(sex);
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        vip.setRegTime(String.valueOf(simpleDateFormat.format(new Date())));
+
+        Vip vip1=vipService.queryUserByName(vip);
+        if (vip1==null){
+            Integer result = vipService.insert(vip);
+            if (result > 0) {
                 response.getWriter().write("注册成功！");
-            }else {
+            } else {
                 response.getWriter().write("注册失败！");
             }
-        }else {
-            response.getWriter().write("密码与确认密码不一致");
         }
+
+    }
+
+
+    public void queryUserByUsername(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        //金句：防止中文乱码
+        response.setContentType("textml;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+        String registername = request.getParameter("registername");
+        Vip vip=new Vip();
+        vip.setUserName(registername);
+        Vip vip1 = vipService.queryUserByName(vip);
+        String message = (vip1 == null) ? null : "1";
+        response.getWriter().write(message);
     }
 }
