f4ea25920faf66fc9d35cdaf7ac08af366cfe926
LiHanQin1 <1428613735@qq.com>
2020-12-11 19:00:58 +0800

    简单框架

---
+++ src/com/servlet/FileUploadServlet1.java
@@ -0,0 +1,105 @@
+package com.servlet;
+
+import com.dao.FilmDao;
+import com.dao.UserDao;
+import com.dao.impl.FilmDaoImpl;
+import com.dao.impl.UserDaoImpl;
+import com.entity.Film;
+import com.entity.User;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Lihanqin
+ * @Description
+ * @date 2020-11-27 9:34
+ **/
+@WebServlet("/FileUploadServlet")
+public class FileUploadServlet1 extends HttpServlet {
+    FilmDao filmDao=null;
+    public FileUploadServlet1(){
+        filmDao=new FilmDaoImpl();
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+        //1 先判断上传的数据是否多段数据 （只有是多段的数据，才是文件上传的）
+        if (ServletFileUpload.isMultipartContent(request)) {
+            // 创建FileItemFactory 工厂实现类
+            FileItemFactory fileItemFactory = new DiskFileItemFactory();
+            //创建用于解析上传数据的工具类
+            ServletFileUpload servletFileUpload = new ServletFileUpload(fileItemFactory);
+            Film film=new Film();
+            try {
+                //解析上传的数据 得到每一个表单项FileItem
+                List<FileItem> list = servletFileUpload.parseRequest(request);
+                //循环判断，每一个表单项 是普通类型 还是上传的文件
+                for (FileItem fileItem :
+                        list) {
+                    if (fileItem.isFormField()) {
+                        //普通表单项
+                        System.out.println("普通表单项 name = " + fileItem.getFieldName());
+                        //参数UTF-8解决乱码
+                        System.out.println("value = " + fileItem.getString());
+                        String str=fileItem.getFieldName();
+                        switch (fileItem.getFieldName()){
+                            case "MovieName":
+                                film.setMovieName(fileItem.getString("utf-8"));
+                                break;
+                            case "TypeId":
+                                film.setTypeId(Integer.valueOf(fileItem.getString("utf-8")));
+                                break;
+                            case "DaoYan":
+                                film.setDaoYan(fileItem.getString("utf-8"));
+                                break;
+                            case "ZhuYan":
+                                film.setZhuYan(fileItem.getString("utf-8"));
+                                break;
+                            case "ShowTime":
+                                film.setShowTime(fileItem.getString("utf-8"));
+                                break;
+                            case "Content":
+                                film.setContent(fileItem.getString("utf-8"));
+                                break;
+
+
+                        }
+                    } else {
+                        //上传的文件
+                        System.out.println("表单项的name = " + fileItem.getFieldName());
+                        System.out.println("上传的文件名:" + fileItem.getName());
+                        String str=fileItem.getName();
+                        StringBuffer sb=new StringBuffer("D:\\upload\\");
+                        sb.append( fileItem.getName());
+                        String path=sb.toString();
+                        film.setPicAddress(path);
+                        fileItem.write(new File(path));
+
+                    }
+                }
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            int result=filmDao.insert(film);
+        }
+    }
+
+
+}
---
+++ web/list.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 14286
+  Date: 2020/12/11
+  Time: 16:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post" enctype="multipart/form-data" action="/architect/FileUploadServlet">
+    影评名称:<input type="text" name="MovieName" id="MovieName">
+    影评类别:<input type="text" name="TypeId" id="TypeId">
+    导演:<input type="text" name="DaoYan" id="DaoYan">
+    主演:<input type="text" name="ZhuYan" id="ZhuYan">
+    上影年份:<input type="text" name="ShowTime" id="ShowTime">
+    图片:<input type="file" name="PicAddress" id="PicAddressTypeId">
+    详细介绍:<input type="text" name="Content" id="Content">
+    添加时间:<input type="text" name="AddTime" id="AddTime">
+    <input type="submit">
+</form>
+</body>
+</html>
